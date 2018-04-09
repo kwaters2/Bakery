14478c091b0251a551db6a8684f3ad9a74ae7b9d
serversideclass <36110565+serversideclass@users.noreply.github.com>
2018-04-09 03:55:49 +0100

    Add files via upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325143834) do
+ActiveRecord::Schema.define(version: 20180406212019) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 20180325143834) do
     t.datetime "updated_at", null: false
     t.string "category_id"
     t.string "—force"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
